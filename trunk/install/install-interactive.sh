684f145917d86a6e821890a8185a9be3942af236
张浩斌 <newsclan@gmail.com>
2017-01-25 13:08:13 +0800

    few package's name

--- trunk/install/install-interactive.sh
+++ trunk/install/install-interactive.sh
@@ -27,8 +27,8 @@ fi
 #try install tools
 if uname -a|grep 'Ubuntu\|Debian'
 then 
-sudo apt-get install make flex g++ clang libmysql++-dev php apache2 mysql-server php-xml php-mysql php-gd php-cli mono-gmcs subversion php-mbstring
-sudo apt-get install make flex g++ clang libmysql++-dev php7.0 apache2 mysql-server php7.0-xml php7.0-mysql php7.0-gd php7.0-cli mono-gmcs php7.0-mbstring subversion  redis-server php7.0-redis
+sudo apt-get install make flex g++ clang libmysql++-dev php apache2 mysql-server php-xml php-mysql php-gd php-cli mono-mcs subversion php-mbstring
+sudo apt-get install make flex g++ clang libmysql++-dev php apache2 mysql-server php-xml php-mysql php-gd php-cli mono-mcs php-mbstring subversion  redis-server php-redis
 sudo /etc/init.d/mysql start
 HTTP_START="sudo /etc/init.d/apache2 restart"
 

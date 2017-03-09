9fce6be11aa1fec04027bc13a059752d663b2319
csgrandeur <csgrandeur@gmail.com>
2017-03-09 14:47:11 +0800

    修改脚本，上一次推送的脚本启动后不判题

--- trunk/install/install_judge_systemd.sh
+++ trunk/install/install_judge_systemd.sh
@@ -2,5 +2,6 @@
 # After core is successfully compiled
 sudo cp judged.service /lib/systemd/system/judged.service
 sudo ln -s /lib/systemd/system/judged.service /etc/systemd/system/multi-user.target.wants/
+systemctl enable judged.service
 systemctl daemon-reload
 systemctl start judged.service

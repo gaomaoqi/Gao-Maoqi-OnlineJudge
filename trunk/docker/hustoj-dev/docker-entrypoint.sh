80ffd77c398bbb1ae437a5ea4d5bdc323921cdc0
shiningrise <shiningrise@gmail.com>
2018-02-26 16:50:31 +0800

    删除无用代码

--- trunk/docker/hustoj-dev/docker-entrypoint.sh
+++ trunk/docker/hustoj-dev/docker-entrypoint.sh
@@ -22,12 +22,6 @@ DIRECTORY="/data/config"
 if [ ! -d $DIRECTORY ]; then
 	cp -R  /home/judge/src/web/config /data
 fi
-if [ ! -f "/data/config/system.conf" ]; then
-	cp /home/judge/src/web/config/system.conf /data/config
-fi
-if [ ! -f "/data/config/root.conf" ]; then
-	cp /home/judge/src/web/config/root.conf /data/config
-fi
 mount --bind $DIRECTORY /home/judge/src/web/config
 
 DIRECTORY="/data/mysql"
@@ -40,24 +34,12 @@ if [  -f "/data/judge.conf" ]; then
 	mv /data/judge.conf /data/etc
 fi
 
-#if [  -f "/data/db_info.inc.php" ]; then
-#	mv /data/db_info.inc.php /data/config
-#fi
-#if [  -f "/data/config/db_info.inc.php" ]; then
-#	cp /data/config/db_info.inc.php /home/judge/src/web/include/db_info.inc.php
-#else
-#	cp /home/judge/src/web/include/db_info.inc.php /data/config/db_info.inc.php
-#fi
-
 cp /data/etc/judge.conf /data/config
 
 chmod 775 -R /data/data 
 chgrp -R www-data /data/data
 chmod 770 -R 		/data/upload /data/config #/data/judge.conf /data/db_info.inc.php
 chgrp -R www-data 	/data/upload /data/config #/data/judge.conf /data/db_info.inc.php
-#/home/judge/src/web/config 
-#/home/judge/src/web/config
-#chown -R mysql:mysql /var/lib/mysql 
 chown -R mysql:mysql /data/mysql/
 
 service mysql start

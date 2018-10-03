3183dee9dfdbbcc9d89c4db4080c880b23f58524
张浩斌 <newsclan@gmail.com>
2018-10-03 18:29:51 +0800

    Update and rename install-ubuntu14.04.sh to install-raspbian8.sh

--- trunk/install/install-raspbian8.sh
+++ trunk/install/install-raspbian8.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get install -y subversion
 /usr/sbin/useradd -m -u 1536 judge
 cd /home/judge/
-svn co https://github.com/zhblue/hustoj/trunk/trunk/ src
+svn co https://github.com/zhblue/hustoj/branches/rpi/trunk  src
 apt-get install -y make flex g++ clang libmysqlclient-dev libmysql++-dev php5-fpm php5-memcache memcached nginx mysql-server php5-mysql php5-gd fp-compiler openjdk-7-jdk
 USER=`cat /etc/mysql/debian.cnf |grep user|head -1|awk  '{print $3}'`
 PASSWORD=`cat /etc/mysql/debian.cnf |grep password|head -1|awk  '{print $3}'`
